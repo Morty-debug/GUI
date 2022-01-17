eca6626bccc118e2b1492fa7cd511d290ef34258
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-16 19:44:06 -0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+	g "github.com/AllenDang/giu"
+)
+
+var (
+	sumando1 string
+	sumando2 string
+	suma string
+)
+
+func formulario() {
+	g.SingleWindow().Layout(
+		g.PrepareMsgbox(),
+		g.Row(
+			g.Label("Ingrese un numero: "),
+			g.InputText(&sumando1),
+		),
+		g.Row(
+			g.Label("Ingrese un numero: "),
+			g.InputText(&sumando2),
+		),
+		g.Button(" Realizar la suma de dos numeros y mostrarlo ").OnClick(sumar),
+		g.Row(
+			g.Label("El resultado es: "),
+			g.InputText(&suma),
+		),	
+	)
+}
+
+func sumar() {
+	n1, err := strconv.ParseFloat(sumando1, 64)
+	if err != nil {
+		g.Msgbox("Error", "Ingrese un numero")
+		return
+	}
+	n2, err := strconv.ParseFloat(sumando2, 64)
+	if err != nil {
+		g.Msgbox("Error", "Ingrese un numero")
+		return
+	}
+	total := n1 + n2
+	suma = fmt.Sprint(total)
+}
+
+func main() {
+	ventana := g.NewMasterWindow("suma de dos numeros", 335, 125, 0)
+	ventana.Run(formulario)
+}
